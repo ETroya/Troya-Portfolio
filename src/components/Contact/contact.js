06849152eb3b259baf12cf55ad2ad4eb0f29e514
Edwin Troya <edtroyajr@gmail.com>
2021-08-01 16:03:43 -0700

    linked to heroku

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Grid, Cell } from "react-mdl";
+import { Grid, Cell, List, ListItem, ListItemContent } from "react-mdl";
 import style from "./stlye.css";
 import Glasses from "../../Assets/images/me.png";
 
@@ -11,11 +11,40 @@ class Contact extends Component {
           <Cell col={6}>
             <h2>Edwin Troya</h2>
             <img src={Glasses} alt="Photo of me" styl={{ height: "250px" }} />
-            <p style={{width: "75%", margin: "auto", paddingTop: "1em"}}> born and raised in New Jersey. After graduating high school, he was very passionate about law enforcement. He persuaded his education in Criminal Justice at Caldwell University.
-            After a few years, Edwin found another interest in Web Developing. He started by self-learning HTML and CSS. He was looking at FreeCodeCamp.org and Codecademy.com. Once Edwin felt that he had a better understanding, he enrolled in the Full-Stack Development program at the UCR extension.  After completing the course, Edwin Troya was now a Full-Stack Developer. 
-            Please feel free to contact Edwin for future developing projects.</p>
+            <p style={{ width: "75%", margin: "auto", paddingTop: "1em" }}>
+              {" "}
+              born and raised in New Jersey. After graduating high school, he
+              was very passionate about law enforcement. He persuaded his
+              education in Criminal Justice at Caldwell University. After a few
+              years, Edwin found another interest in Web Developing. He started
+              by self-learning HTML and CSS. He was looking at FreeCodeCamp.org
+              and Codecademy.com. Once Edwin felt that he had a better
+              understanding, he enrolled in the Full-Stack Development program
+              at the UCR extension. After completing the course, Edwin Troya was
+              now a Full-Stack Developer. Please feel free to contact Edwin for
+              future developing projects.
+            </p>
           </Cell>
-          <Cell col={6}>Half page</Cell>
+          <Cell col={6}>
+            <h2>Contact Me</h2>
+            <hr />
+            <div className="contact-list">
+            <List>
+              <ListItem>
+                <ListItemContent style={{fontSize:"25px", fontFamily: "Anton"}}>
+                  <i className="fa fa-envelope" aria-hidden="true"/>
+                  edtroyajr@gmail.com</ListItemContent>
+              </ListItem>
+              <ListItem>
+                <ListItemContent icon="person"></ListItemContent>
+              </ListItem>
+              <ListItem>
+                <ListItemContent icon="person"></ListItemContent>
+              </ListItem>
+            </List>
+            </div>
+            
+          </Cell>
         </Grid>
       </div>
     );
